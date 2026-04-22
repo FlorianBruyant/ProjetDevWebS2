1390bda61a15cc580f08f2ea42c1b07f65ea0dc1
MOUSSBEKK <146588891+MOUSSBEKK@users.noreply.github.com>
2026-04-22 22:23:02 +0200

    GROS COMMIT
    Des histoires de filtres de BDD par rapport a letat des objets

--- backend/map/views.py
+++ backend/map/views.py
@@ -1,4 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny  # <-- On change pour "AllowAny"
 
 from .models import Vehicule, Zone
 from .serializers import VehiculeSerializer, ZoneSerializer
@@ -12,3 +14,9 @@ class ZoneViewSet(viewsets.ModelViewSet):
 class VehiculeViewSet(viewsets.ModelViewSet):
     queryset = Vehicule.objects.all()
     serializer_class = VehiculeSerializer
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["est_actif", "en_panne", "point_actuel__zone"]
+
+    # On autorise tout le monde le temps de vérifier que les filtres marchent
+    permission_classes = [AllowAny]
