46943333320171ad00e90e35bdb0c7126597d82c
MOUSSBEKK <146588891+MOUSSBEKK@users.noreply.github.com>
2026-04-22 21:31:28 +0200

    Admin panel BDD
    BDD acces admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import ActionLog, CustomUser
+
+# On enregistre l'utilisateur personnalisé et les logs
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(ActionLog)
