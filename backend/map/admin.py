c4878c23eb84305a640cee5949715bc1fb3b47af
MOUSSBEKK <146588891+MOUSSBEKK@users.noreply.github.com>
2026-04-22 21:28:01 +0200

    Superuser
    Admin creation

--- backend/map/admin.py
+++ backend/map/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Feu, Incident, Parking, Point, Route, Vehicule, Zone
+
+# On enregistre tous nos modèles pour qu'ils soient visibles dans l'admin
+admin.site.register(Point)
+admin.site.register(Zone)
+admin.site.register(Feu)
+admin.site.register(Parking)
+admin.site.register(Vehicule)
+admin.site.register(Route)
+admin.site.register(Incident)
