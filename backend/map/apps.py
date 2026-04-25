1a313d61a4262a087c66af2f99de3dfdc887bc31
Florian <bruyantflorian2@gmail.com>
2026-04-25 23:45:33 +0200

    fix: delete données de simu de plus de 24 h

--- backend/map/apps.py
+++ backend/map/apps.py
@@ -1,7 +1,9 @@
 import threading
 import time
+from datetime import timedelta
 
 from django.apps import AppConfig
+from django.utils import timezone
 
 
 class MapConfig(AppConfig):
@@ -11,6 +13,7 @@ class MapConfig(AppConfig):
     def ready(self):
         # On importe ici pour éviter les erreurs de chargement au démarrage
         from .management.commands.simuler_activite import Command
+        from .models import HistoriqueObjet
 
         def run_simulation_conso():
             sim = Command()
@@ -19,6 +22,12 @@ class MapConfig(AppConfig):
                 time.sleep(30)
                 try:
                     sim.handle()
+
+                    # NETTOYAGE : On ne garde que les dernières 24 heures
+                    limite_temps = timezone.now() - timedelta(hours=24)
+                    HistoriqueObjet.objects.filter(
+                        date_mesure__lt=limite_temps
+                    ).delete()
                     print(" Simulation auto : Historique mis à jour.")
                 except Exception as e:
                     print(f"Erreur simulation : {e}")
@@ -31,9 +40,7 @@ class MapConfig(AppConfig):
             from .management.commands.simulateur_feux import demarrer_simulation_feux
 
             # 1. On crée et on lance le thread pour les FEUX
-            thread_feux = threading.Thread(target=demarrer_simulation_feux, daemon=True)
-            thread_feux.start()
+            threading.Thread(target=demarrer_simulation_feux, daemon=True).start()
 
             # 2. On crée et on lance le thread pour la CONSOMMATION
-            thread_conso = threading.Thread(target=run_simulation_conso, daemon=True)
-            thread_conso.start()
+            threading.Thread(target=run_simulation_conso, daemon=True).start()
